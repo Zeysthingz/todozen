067a8fbaa4bd8f72ae132555e6d00e4c98aec016
Zeynep <zeynepakbalikk@gmail.com>
2023-08-15 22:54:34 +0300

    created index page

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import TodoItem
+
 
 # Create your views here.
+
+# Create home page view
+def index(request):
+    context = {}
+    return render(request, 'todo/todo_list.html', context)
