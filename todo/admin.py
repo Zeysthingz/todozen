0a5ada7a863475b12e7623b65559c7d32fc03611
Zeynep <zeynepakbalikk@gmail.com>
2023-08-15 22:06:43 +0300

    created core html structure

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import TodoItem
+
+
+# Show the TodoItem model fields in the admin page
+class TodoItemAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'is_completed',
+        # 'date_started',
+        # 'date_completed',
+    ]
+
+
+admin.site.register(TodoItem, TodoItemAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class TodoItem(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True, null=True)
+    date_started = models.DateTimeField(auto_now_add=True)
+    date_completed = models.DateTimeField(auto_now=True)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
