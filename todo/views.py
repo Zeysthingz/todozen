a4728d6e1fa18ee7e61d25f85fe8f57b36701261
Zeynep <zeynepakbalikk@gmail.com>
2023-08-18 00:10:34 +0300

    added login decorator

--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render, get_object_or_404
 from .models import TodoItem, Category
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
-from django.http import Http404
 
-
-# Create your views here.
-
-# Create home page view
+@login_required(login_url='/admin/login/')
 def index(request):
     queryset = TodoItem.objects.filter(is_completed=False, user=request.user)
     context = {
@@ -15,15 +12,17 @@ def index(request):
     return render(request, 'todo/todo_list.html', context)
 
 
+@login_required(login_url='/admin/login/')
 # Get items by id from database
 def todo_detail(request, id):
-    query = get_object_or_404(TodoItem, pk=id,user=request.user)
+    query = get_object_or_404(TodoItem, pk=id, user=request.user)
     context = {
         'query': query,
     }
     return render(request, 'todo/todo_detail.html', context)
 
 
+@login_required(login_url='/admin/login/')
 def category(request, slug):
     category = get_object_or_404(Category, slug=slug)
     queryset = TodoItem.objects.filter(is_completed=False, category=category, user=request.user)
